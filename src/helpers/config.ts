1b1ca65daf11b5530d694ad473a10a37588e5466
Mauricio Zanetti Filho <contact@thinkincoin.com>
2021-09-19 06:04:21 -0300

    Update config.ts

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -3,6 +3,6 @@ import config from '@/config.json';
 let id = 'master';
 const domainName = window.location.hostname;
 if (domainName.includes('localhost:')) id = 'local';
-if (domainName === 'beta.vote.balancer.finance') id = 'develop';
+if (domainName === 'makiswap-dev.thinkincoin.com') id = 'develop';
 
 export default config[id];
